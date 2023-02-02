0e9e1ccb63e6af6a3620ecb215b98a6d6a9a1828
Niranjan Subedi <niranjansubedi@Niranjans-MacBook-Pro.local>
2023-02-02 15:26:47 +0000

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# rn-contacts
+
+uses native contact picker apis to select and update contacts from react native based applications
+
+## Installation
+
+```sh
+npm install rn-contacts
+```
+
+## Usage
+
+```js
+import { multiply } from 'rn-contacts';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ android/src/main/java/com/rncontacts/RnContactsModule.java
@@ -0,0 +1,32 @@
+package com.rncontacts;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = RnContactsModule.NAME)
+public class RnContactsModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "RnContacts";
+
+  public RnContactsModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
